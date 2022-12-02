43965a88f4a84a098453087e0639e6cdf0faf544
Oleg Smolyakov <osmo@Olegs-MacBook-Pro.local>
2022-12-02 16:59:04 -0600

    Added checkoutPage.js

--- cypress/e2e/test-spec.cy.js
+++ cypress/e2e/test-spec.cy.js
@@ -10,4 +10,28 @@ describe('API tests', ()=>{
         expect(result).to.have.property("status", 200)
       })
     })
-  });
+
+describe('Registration', () => {
+  it("tests duplicated email can't be registered", () => {
+    onLoginPage.registerNewUser();
+    cy.contains('The email address is already in use by another account.')
+      .should('be.visible');
+    })
+  })  
+
+describe('Login', () => {
+  it('tests that user can login with valid credentials', () => {
+    onLoginPage.login();
+    cy.contains('Logout').should('be.visible');
+    })
+  })  
+
+describe('Registration', () => {
+  it("Tests duplicated email can't be registered", () => {
+    onLoginPage.logout();
+    onLoginPage.registerNewUser();
+    cy.contains('The email address is already in use by another account.')
+      .should('be.visible');
+    })
+  })
+})
---
+++ cypress/support/page-objects/checkoutPage.js
@@ -0,0 +1,27 @@
+export class CheckoutPage {
+  
+  checkProductIsInCart(productName) {
+    cy.get('[data-th="Product"]')
+      .contains(productName)
+      .should('be.visible');
+  };
+
+  checkProductIsNotInCart(productName) {
+    cy.get('[data-th="Product"]')
+      .contains(productName)
+      .should('not.exist');
+  };
+
+  checkoutCart() {
+    cy.get('#cart')
+    .contains('Checkout')
+    .click();
+  };
+
+  removeProduct() {
+    cy.get('.fa-trash-o')
+    .first()
+    .click();
+  };
+}
+  export const onCheckoutPage = new CheckoutPage();
