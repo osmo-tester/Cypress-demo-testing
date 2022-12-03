b15f5df1de8cf10281c4f3351418603b1feac344
Oleg Smolyakov <osmo@Olegs-MacBook-Pro.local>
2022-12-02 20:50:46 -0600

    Test corrections and added tests numbers

--- cypress/e2e/test-spec.cy.js
+++ cypress/e2e/test-spec.cy.js
@@ -9,83 +9,83 @@ beforeEach(() => {
 })
 
 describe('API tests', ()=>{
-    it('the site is opening with status 200', () =>{
-      cy.request("https://vuejs-shopping-cart.coddeine.com/").then((result) => {
-        expect(result).to.have.property("status", 200)
-      })
+  it(`#1 the site is opening with status 200`, () =>{
+    cy.request("https://vuejs-shopping-cart.coddeine.com/").then((result) => {
+      expect(result).to.have.property("status", 200)
     })
+  })
+});  
 
 describe('Registration', () => {
-  it("tests duplicated email can't be registered", () => {
+  it(`#2 tests duplicated email can't be registered`, () => {
     onLoginPage.registerNewUser();
-    cy.contains('The email address is already in use by another account.')
-      .should('be.visible');
-    })
-  })  
+    onLoginPage.duplicatedAdderessNotification();
+  })
+});  
 
 describe('Login', () => {
-  it('tests that user can login with valid credentials', () => {
+  it(`#3 tests that user can login with valid credentials`, () => {
     onLoginPage.login();
-    cy.contains('Logout').should('be.visible');
-    })
-  })  
+    onLoginPage.loginPageIsOpened();
+  })
+});  
 
 describe('Registration', () => {
-  it("Tests duplicated email can't be registered", () => {
+  it(`#4 tests duplicated email can't be registered`, () => {
     onLoginPage.logout();
     onLoginPage.registerNewUser();
     cy.contains('The email address is already in use by another account.')
       .should('be.visible');
-    })
   })
+});
 
 describe('Shopping cart/Checkout', () => {
-  it('tests that product can be added to the cart', () => {
+  it(`#5 tests that product can be added to the cart`, () => {
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.openCart();
     onCheckoutPage.checkProductIsInCart("macbook Retina 13.3' ME662 (2013)");
-  })
+  });
 
-  it('test that the shopping cart is cleared after user logged out', ()=>{
+  it(`#6 test that the shopping cart is cleared after user logged out`, ()=>{
     onLoginPage.login();
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     cy.contains('.btn', 'Checkout').should('contain', '1 ($ 2399)');
-    cy.wait(5000);
     onLoginPage.logout();
     cy.contains('.btn', 'Checkout').should('contain', '0 ($ 0)');
-  })
+  });
 
-  it('tests that multipple products can be added to the cart', () => {
+  it(`#7 tests that multipple products can be added to the cart`, () => {
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.addProductToCart("Macbook Pro 13.3' Retina MF841LL/A");
     onMainPage.openCart();
     onCheckoutPage.checkProductIsInCart("macbook Retina 13.3' ME662 (2013)");
     onCheckoutPage.checkProductIsInCart("Macbook Pro 13.3' Retina MF841LL/A");
-  })
+  });
 
-  it('tests that product can be removed from cart', () => {
+  it(`#8 tests that product can be removed from cart`, () => {
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.openCart();
     onCheckoutPage.checkProductIsInCart("macbook Retina 13.3' ME662 (2013)");
     onCheckoutPage.removeProduct();
     onCheckoutPage.checkProductIsNotInCart("macbook Retina 13.3' ME662 (2013)");
-  })
+  });
 
-  it('tests that order can be succesfully checkout', () => {
+  it(`#9 tests that order can be succesfully checkout`, () => {
     onLoginPage.login();
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.openCart();
     onCheckoutPage.checkoutCart();
     cy.contains('Your order has been successfully processed!').should('be.visible');
-  })
+  });
 
-  it("tests that quantity of products can't be negative", () => {
+  // This test should fall, as expected
+  it(`#10 tests that quantity of products can't be negative`, () => {
     cy.log('This is a BUG and test is expected to fail');
     onLoginPage.login();
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.openCart();
     cy.get('input').clear().type('-1');
     cy.get('input').should('not.have.value', '-1');
-  })
-})
-})
+  });
+});
+
--- cypress/support/page_objects/mainPage.js
+++ cypress/support/page_objects/mainPage.js
@@ -2,16 +2,17 @@ export class MainPage {
   
   addProductToCart(productName) {
     cy.contains(productName)
-    .parent('.card-title')
-    .siblings('.row')
-    .children('.col-6')
-    .contains('Add to cart').click()
-    cy.wait(3000)
-  }
+      .parent('.card-title')
+      .siblings('.row')
+      .children('.col-6')
+      .contains('Add to cart')
+      .click()
+    cy.wait(3000)  
+  };
 
   openCart() {
     cy.contains('Checkout').click()
-  }
-}
+  };
+};
 
 export const onMainPage = new MainPage();
