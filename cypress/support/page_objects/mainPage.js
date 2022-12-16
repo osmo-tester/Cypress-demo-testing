cd411b7ece7af85a00ba386934e75cb122fea787
Oleg Smolyakov <osmo@Olegs-MacBook-Pro.local>
2022-12-16 10:52:04 -0600

    Added new function to the page-objects

--- cypress/support/page_objects/mainPage.js
+++ cypress/support/page_objects/mainPage.js
@@ -1,27 +1,30 @@
 export class MainPage {
-  
   addProductToCart(productName) {
     cy.contains(productName)
-      .parent('.card-title')
-      .siblings('.row')
-      .children('.col-6')
-      .contains('Add to cart')
-      .click()
-    cy.wait(3000)  
-  };
+      .parent(".card-title")
+      .siblings(".row")
+      .children(".col-6")
+      .contains("Add to cart")
+      .click();
+    cy.wait(3000);
+  }
 
   openCart() {
-    cy.contains('Checkout').click()
-  };
+    cy.contains("Checkout").click();
+  }
 
   confirmCheckoutInfo() {
-    cy.contains('.btn', 'Checkout')
+    cy.contains(".btn", "Checkout")
       // .should('contain', '1 ($ 2399)');
-      .invoke('text')
-      .then($txt1 => {
-        expect($txt1).to.equal($txt1)
-      })
-  };
-};
+      .invoke("text")
+      .then(($txt1) => {
+        expect($txt1).to.equal($txt1);
+      });
+  }
+
+  typeNegativeValue() {
+    cy.get("input").clear().type("-1").should("not.have.value", "-1");
+  }
+}
 
 export const onMainPage = new MainPage();
