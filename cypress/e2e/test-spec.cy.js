5f83ea2802e3cac11c8256869d67ceb150983e5b
Oleg Smolyakov <osmo@Olegs-MacBook-Pro.local>
2023-01-30 13:07:13 -0600

    Negative example

--- cypress/e2e/test-spec.cy.js
+++ cypress/e2e/test-spec.cy.js
@@ -1,22 +1,22 @@
 /// <reference types = "cypress"/>
 
-import { onLoginPage } from "../support/page_objects/loginPage";
-import { onMainPage } from "../support/page_objects/mainPage";
-import { onCheckoutPage } from "../support/page_objects/checkoutPage";
+import { onLoginPage } from '../support/page_objects/loginPage';
+import { onMainPage } from '../support/page_objects/mainPage';
+import { onCheckoutPage } from '../support/page_objects/checkoutPage';
 
 beforeEach(() => {
-  cy.visit("/");
+  cy.visit('/');
 });
 
-describe("API tests", () => {
+describe('API tests', () => {
   it(`#1 the site is opening with status 200`, () => {
-    cy.request("https://vuejs-shopping-cart.coddeine.com/").then((result) => {
-      expect(result).to.have.property("status", 200);
+    cy.request('https://vuejs-shopping-cart.coddeine.com/').then((result) => {
+      expect(result).to.have.property('status', 200);
     });
   });
 });
 
-describe("Registration", () => {
+describe('Registration', () => {
   it(`#2 tests that the user is able to be registered`, () => {
     onLoginPage.registerNewUser();
     onLoginPage.confirmRegistration();
@@ -28,16 +28,16 @@ describe("Registration", () => {
   });
 });
 
-describe("Login", () => {
+describe('Login', () => {
   it(`#4 tests that user is able login with valid credentials`, () => {
     onLoginPage.login();
     onLoginPage.loginPageIsOpened();
   });
 });
 
-describe("Shopping cart/Checkout", () => {
-  it(`#5 tests that product can be added to the cart`, () => {
-    onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
+describe('Shopping cart/Checkout', () => {
+  it.only(`#5 tests that product can be added to the cart`, () => {
+    onMainPage.addProductToCart("macbook Retina 13.3' ME669 (2013)");
     onMainPage.openCart();
     onCheckoutPage.checkProductIsInCart("macbook Retina 13.3' ME662 (2013)");
   });
@@ -76,7 +76,7 @@ describe("Shopping cart/Checkout", () => {
 
   // This test should fall, as expected
   it.skip(`#10 tests that quantity of products can't be negative`, () => {
-    cy.log("This is a BUG and test is expected to fail");
+    cy.log('This is a BUG and test is expected to fail');
     onLoginPage.login();
     onMainPage.addProductToCart("macbook Retina 13.3' ME662 (2013)");
     onMainPage.openCart();
