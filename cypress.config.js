67c967989054ad1d016d05f7e534189c5182839e
Oleg Smolyakov <osmo@Olegs-MacBook-Pro.local>
2023-01-30 14:03:17 -0600

    Changed ci-cypress-tests.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 960,
-  blockHosts: ["google-analytics.com", "googletagmanager.com"],
+  blockHosts: ['google-analytics.com', 'googletagmanager.com'],
   defaultCommandTimeout: 2000,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://vuejs-shopping-cart.coddeine.com",
     experimentalSessionSupport: true,
   },
 
   env: {
-    username: "alexsmol.qa+test@gmail.com",
-    password: "123456"
-  }
+    username: 'alexsmol.qa+test@gmail.com',
+    password: '123456',
+  },
 });
